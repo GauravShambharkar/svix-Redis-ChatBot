59a1171402562d33ef210908056feaee704d5c84
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-10-19 17:15:12 +0530

    ImplimentedRedis

--- src/Controllers/gemini.ts
+++ src/Controllers/gemini.ts
@@ -1,33 +1,57 @@
 import type { Request, Response } from "express";
 import { GoogleGenAI } from "@google/genai";
+import { createClient } from "redis";
 import dotenv from "dotenv";
+
 dotenv.config();
 
-// gemini secret api key
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
-// main function
 const gemini = async (req: Request, res: Response) => {
   const { newMsg } = req.body;
+
   if (!GEMINI_API_KEY) {
-    res.status(500).send({
+    return res.status(500).send({
       err: "Missing GEMINI_API_KEY in environment variables",
     });
-    throw new Error("Missing GEMINI_API_KEY in environment variables");
   }
 
-  const ai = new GoogleGenAI({
-    apiKey: GEMINI_API_KEY,
-  });
+  // Connect Redis
+  const redisClient = createClient();
+  redisClient.on("error", (err) => console.log("Redis Client Error", err));
+  await redisClient.connect();
+
+  const redisKey = `chat: ${newMsg}`;
+
+  const cachedChat = await redisClient.get(redisKey);
+  if (cachedChat) {
+    return res.send({
+      cachedChat: true,
+      geminiResponse: cachedChat,
+    });
+  }
+
+  // non cached response
+  const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 
   const geminiResponse = await ai.models.generateContent({
     model: "gemini-2.0-flash-001",
     contents: newMsg,
   });
 
+  if (!geminiResponse.text) {
+    return res.status(500).send({
+      err: "Gemini response text is undefined",
+    });
+  }
 
-  res.send({
-    geminiResponse: geminiResponse.text,
+  const cleanText = geminiResponse.text.replace(/\n+/g, " ").trim();
+
+  await redisClient.set(redisKey, cleanText, { EX: 3600 }); // expires in 1 hour
+
+  return res.send({
+    cachedChat: false,
+    geminiResponse: cleanText,
   });
 };
 
--- src/Controllers/readChatController.ts
+++ src/Controllers/readChatController.ts
@@ -1,6 +1,15 @@
 import { type Request, type Response } from "express";
+import { createClient } from "redis";
 
-export const readChatController = (req: Request, res: Response) => {
+export const readChatController = async (req: Request, res: Response) => {
+  
+  const chatCache = createClient();
+
+  const chatExist = await chatCache.get("chats");
+
+  if (chatExist) {
+  }
+
   res.send({
     payload: "payload will be here",
   });
